e9f192ae00e7572fb78692bf9eac144da6938741
ohdongpodong <ohdongpodong@gmail.com>
2019-10-10 23:46:02 +0900

    inflearnSpring1

---
+++ src/main/java/chap5/MainClass.java
@@ -0,0 +1,23 @@
+package chap5;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        /*
+        TransportationWalk transpotationWalk = new TransportationWalk();
+        transpotationWalk.move();
+        */
+
+        // # 스프링 컨테이너 접근 방법
+
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+
+        TransportationWalk transportationWalk = ctx.getBean("tWalk", TransportationWalk.class);
+        transportationWalk.move();
+
+        ctx.close();
+    }
+
+}
---
+++ src/main/java/chap5/TransportationWalk.java
@@ -0,0 +1,8 @@
+package chap5;
+
+public class TransportationWalk {
+
+    public void move(){
+        System.out.println("도보로 이동 합니다.");
+    }
+}
